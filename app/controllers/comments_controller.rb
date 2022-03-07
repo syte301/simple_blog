fc73cc887f558fb007f07f457c45693653ed5829
ww301347 <ww301347@gmail.com>
2022-03-07 22:15:55 +0600

    Init

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  #http_basic_authenticate_with name: "admin", password: "1q2"
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:username, :body)
+    end
+end
